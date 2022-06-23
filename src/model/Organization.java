e8b224c89856b85c849f490b597e0197a4f8ee44
Carlos Pedraza <capedraza@gmail.com>
2022-06-23 22:51:18 +0200

    Update 23-06-2022 PM 2

--- src/model/Organization.java
+++ src/model/Organization.java
@@ -1,10 +1,155 @@
 
 package model;
 
+import java.util.Date;
+
 public class Organization {
     private String id;
     private String legalName;
-    private String comercilaName;
+    private String comercialName;
+    private Date dateRegister;
+    private String type;
+    private boolean state;
+    private String notes;
+    private String phone;
+    private String email;
+    private String address;
+    private String city;
+    private String coutry;
+    private String postalCode;
+
+    public Organization() {
+    }
+
+    public Organization(String id, String legalName, String comercialName, Date dateRegister, String type, boolean state, String notes, String phone, String email, String address, String city, String coutry, String postalCode) {
+        this.id = id;
+        this.legalName = legalName;
+        this.comercialName = comercialName;
+        this.dateRegister = dateRegister;
+        this.type = type;
+        this.state = state;
+        this.notes = notes;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+        this.city = city;
+        this.coutry = coutry;
+        this.postalCode = postalCode;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLegalName() {
+        return legalName;
+    }
+
+    public void setLegalName(String legalName) {
+        this.legalName = legalName;
+    }
+
+    public String getComercialName() {
+        return comercialName;
+    }
+
+    public void setComercialName(String comercialName) {
+        this.comercialName = comercialName;
+    }
+
+    public Date getDateRegister() {
+        return dateRegister;
+    }
+
+    public void setDateRegister(Date dateRegister) {
+        this.dateRegister = dateRegister;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public boolean isState() {
+        return state;
+    }
+
+    public void setState(boolean state) {
+        this.state = state;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCoutry() {
+        return coutry;
+    }
+
+    public void setCoutry(String coutry) {
+        this.coutry = coutry;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Organization{" + "id=" + id + ", legalName=" + legalName + ", comercialName=" + comercialName + ", dateRegister=" + dateRegister + ", type=" + type + ", state=" + state + ", notes=" + notes + ", phone=" + phone + ", email=" + email + ", address=" + address + ", city=" + city + ", coutry=" + coutry + ", postalCode=" + postalCode + '}';
+    }
+    
+    
+    
+    
+    
     
     
     
